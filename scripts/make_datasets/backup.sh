9b8bfae28f40cdb715832cd78d2167efb3193c78
mawilson1234 <michael.a.wilson@yale.edu>
2022-10-02 12:57:08 -0400

    add backup script

---
+++ scripts/make_datasets/backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=make-seq2seq-datasets
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=20:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/seq2seq-datasets
+
+python make_datasets.py
--- scripts/make_datasets/make_datasets.sh
+++ scripts/make_datasets/make_datasets.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=make-seq2seq-datasets
+#SBATCH --job-name=backup-seq2seq-datasets
 #SBATCH --output=joblogs/%x_%j.txt
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=30GB
-#SBATCH --time=20:00:00
-#SBATCH --gpus=v100:1
-#SBATCH --partition=gpu
+#SBATCH --time=00:10:00
 #SBATCH --mail-type=END,FAIL,INVALID_DEPEND
 
-module load CUDA
-module load cuDNN
-module load miniconda
-
-source activate /gpfs/gibbs/project/frank/ref4/conda_envs/seq2seq-datasets
-
-python make_datasets.py
+git add --all .
+git commit -m "add new datasets"
+git push origin main
