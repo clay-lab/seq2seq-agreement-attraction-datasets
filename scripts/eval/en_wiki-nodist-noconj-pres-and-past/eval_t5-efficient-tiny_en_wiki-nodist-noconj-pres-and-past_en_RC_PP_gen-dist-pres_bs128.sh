0b3f1fda6848996915493384aadaa143331e9093
Michael Wilson <michael.a.wilson@yale.edu>
2022-11-10 00:15:27 -0500

    update scripts

---
+++ scripts/eval/en_wiki-nodist-noconj-pres-and-past/eval_t5-efficient-tiny_en_wiki-nodist-noconj-pres-and-past_en_RC_PP_gen-dist-pres_bs128.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=t5-efficient-tiny-eval-tense-en_wiki-nodist-noconj-pres-and-past
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=10:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/py38-agratt
+
+python core/run_seq2seq.py \
+	--model_name_or_path 't5-efficient-tiny' \
+	--do_learning_curve \
+	--task translation_src_to_tgt \
+	--train_file data/en_wiki-nodist-noconj-pres-and-past/en_wiki-nodist-noconj-pres-and-past_train.json.gz \
+	--validation_file data/en_RC_PP_gen-dist-pres/en_RC_PP_gen-dist-pres_test.json.gz \
+	--output_dir outputs/en_wiki-nodist-noconj-pres-and-past/t5-efficient-tiny-finetuning-en_wiki-nodist-noconj-pres-and-past-bs128/ \
+	--per_device_train_batch_size=8 \
+	--per_device_eval_batch_size=16 \
+	--overwrite_output_dir \
+	--predict_with_generate \
