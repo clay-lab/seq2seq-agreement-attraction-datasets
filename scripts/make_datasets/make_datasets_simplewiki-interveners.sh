5890d512d7b903fadd1327a663b6fb2037126fa1
mawilson1234 <michael.a.wilson@yale.edu>
2023-01-24 17:09:23 -0500

    simplewiki datasets

---
+++ scripts/make_datasets/make_datasets_simplewiki-interveners.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=make-seq2seq-datasets-simplewiki-interveners
+#SBATCH --output=joblogs/%x_%j.txt
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB
+#SBATCH --time=12:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+
+module load CUDA
+module load cuDNN
+module load miniconda
+
+source activate /gpfs/gibbs/project/frank/ref4/conda_envs/seq2seq-datasets
+
+python make_datasets.py -o en_simplewiki-interveners
